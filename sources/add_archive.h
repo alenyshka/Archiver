fb58bf5decc2853a2ea7e3e379c2c4f24ff652fa
Mozyr235 <mozyr235@gmail.com>
2015-05-29 19:27:10 +0300

    tekst

--- sources/add_archive.h
+++ sources/add_archive.h
@@ -4,6 +4,18 @@ tree_element *root;
 char* input_text;
 int text_lenght;
 
+char enter_text(char *&input_text, int text_lenght)
+{
+	FILE *f;
+	if (!(f = fopen("input.txt", "a")))
+		return 0;
+	printf("\nEnter the text:");
+	fflush(stdin);
+	gets(input_text);
+	fprintf(f, "%s", input_text);
+	return *input_text;
+}
+
 tree_element create_queue(tree_element *&root, char *&input_text, int text_lenght)
 {
 	for (int i = 0; input_text[i]!='\0'; i++)
